4e197278f823f581105d2780c49ae28d5377c8d8
Steven Dao <iamstevendao@users.noreply.github.com>
2017-12-14 11:16:08 +1100

    done real-time formatting, validate, get result

--- src/components/tel-input.vue
+++ src/components/tel-input.vue
@@ -1,18 +1,25 @@
 <template lang="pug">
-b-input-group
-  b-dropdown(variant="light")
-    template(slot="button-content") 
-      img(:src="activeCountry.icon")
-    b-dropdown-item(v-for="pb in allCountries"
-                    :key="pb['iso2']" 
-                    @click="choose(pb)")
-      img(:src="pb.icon")
-      strong {{ pb.name }}  
-      span +{{ pb.dialCode }}
-  b-form-input
+b-card
+  b-input-group
+    b-dropdown(variant="light")
+      template(slot="button-content") 
+        img(:src="activeCountry.icon")
+      b-dropdown-item(v-for="pb in allCountries"
+                      :key="pb['iso2']" 
+                      @click="choose(pb)")
+        img(:src="pb.icon")
+        strong {{ pb.name }}  
+        span +{{ pb.dialCode }}
+    b-form-input(v-model="phoneNumber"
+                placeholder="Enter your phone number"
+                :state="state"
+                :formatter="format")
+  h1 {{ formattedResult }}
+  h1 {{ parsedResult }}
 </template>
 
 <script>
+import { parse, format, asYouType, isValidNumber } from 'libphonenumber-js';
 import allCountries from '../assets/all-countries';
 
 export default {
@@ -21,13 +28,27 @@ export default {
     return {
       allCountries,
       activeCountry: allCountries[0],
+      phoneNumber: '',
     };
   },
   computed: {
+    formattedResult() {
+      return format(this.phoneNumber, this.activeCountry.iso2, 'International');
+    },
+    parsedResult() {
+      const result = parse(this.phoneNumber, this.activeCountry.iso2);
+      return result.length > 0 ? result : '';
+    },
+    state() {
+      return isValidNumber(this.phoneNumber, this.activeCountry.iso2);
+    },
   },
   methods: {
     choose(country) {
       this.activeCountry = country;
+    },
+    format(value, event) {
+      return new asYouType(this.activeCountry.iso2).input(value);
     },
   },
 };

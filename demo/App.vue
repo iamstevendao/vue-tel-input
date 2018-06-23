29cbee2968bd054e1bf14ec7b7c6582842b876a8
Steven Dao <iamstevendao@gmail.com>
2018-06-23 23:20:29 +1000

    Reformat codebase

--- demo/App.vue
+++ demo/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
     <h1>Telephone Input Vue</h1>
-	<h2 style="color: #999">made with &#x2764; by Steven.</h2>
-	<div style="width: 500px; margin: 20px auto;">
-	  <vue-tel-input @onInput="onInput">
-	  </vue-tel-input>
+    <h2 style="color: #999">made with &#x2764; by Steven.</h2>
+    <div style="width: 500px; margin: 20px auto;">
+      <vue-tel-input @onInput="onInput">
+      </vue-tel-input>
     </div>
-    <div v-if="phone.number" style="color: #e83e8c">
-            <span>Number:
-              <strong>{{ phone.number }}</strong>,&nbsp;
-            </span>
-            <span>Is valid:
-              <strong>{{ phone.isValid }}</strong>,&nbsp;
-            </span>
-            <span>Country:
-              <strong>{{ phone.country }}</strong>
-            </span>
+    <div v-if="phone.number"
+         style="color: #e83e8c">
+      <span>Number:
+        <strong>{{ phone.number }}</strong>,&nbsp;
+      </span>
+      <span>Is valid:
+        <strong>{{ phone.isValid }}</strong>,&nbsp;
+      </span>
+      <span>Country:
+        <strong>{{ phone.country }}</strong>
+      </span>
     </div>
-	</div>
+  </div>
 </template>
 
 <script>
--- src/vue-tel-input.vue
+++ src/vue-tel-input.vue
@@ -1,103 +1,109 @@
 <template>
   <div class="vue-tel-input">
-	<div class="dropdown"  @click="toggleDropdown" v-click-outside="clickedOutside" :class="{open: open}">
-	<span class="selection">
-		<img :src="activeCountry.icon" class="flag" />
-		<span class="dropdown-arrow">
-		{{ open ? '▲' : '▼' }}
-		</span>
-	</span>
-	  <ul v-show="open">
-		  <li class="dropdown-item" v-for="pb in allCountries"
-						   :key="pb['iso2']"
-						   @click="choose(pb)">
-			<img :src="pb.icon" style="width: 25px; margin-right: 5px" />
-			<strong>{{ pb.name }} </strong>
-			<span>+{{ pb.dialCode }}</span>
-		  </li>
-	  </ul>
-	</div>
-	<input v-model="phone"
-				  :placeholder="placeholder"
-				  :state="state"
-				  :formatter="format"
-				  @input="onInput">
-	</input>
+    <div class="dropdown"
+         @click="toggleDropdown"
+         v-click-outside="clickedOutside"
+         :class="{open: open}">
+      <span class="selection">
+        <img :src="activeCountry.icon"
+             class="flag" />
+        <span class="dropdown-arrow">
+          {{ open ? '▲' : '▼' }}
+        </span>
+      </span>
+      <ul v-show="open">
+        <li class="dropdown-item"
+            v-for="pb in allCountries"
+            :key="pb['iso2']"
+            @click="choose(pb)">
+          <img :src="pb.icon"
+               style="width: 25px; margin-right: 5px" />
+          <strong>{{ pb.name }} </strong>
+          <span>+{{ pb.dialCode }}</span>
+        </li>
+      </ul>
+    </div>
+    <input v-model="phone"
+           :placeholder="placeholder"
+           :state="state"
+           :formatter="format"
+           @input="onInput">
   </div>
 </template>
 
 <style scoped>
 :local {
-	--border-radius: 2px;
+  --border-radius: 2px;
 }
 .selection {
-	cursor: pointer;
-	font-size: 0.8em;
-	display: flex;
-	align-items: center;
+  cursor: pointer;
+  font-size: 0.8em;
+  display: flex;
+  align-items: center;
 }
 .vue-tel-input {
-	border-radius: 3px;
-	display: flex;
-	border: 1px solid #bbb;
-	text-align: left;
+  border-radius: 3px;
+  display: flex;
+  border: 1px solid #bbb;
+  text-align: left;
 }
 .vue-tel-input:focus-within {
-	box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgba(102,175,233,.6);
-	border-color: #66afe9;
+  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075),
+    0 0 8px rgba(102, 175, 233, 0.6);
+  border-color: #66afe9;
 }
 input {
-	border: none;
-	border-radius: 0 var(--border-radius) var(--border-radius) 0;
-	width: 100%;
-	outline: none;
-	padding-left: 7px;
+  border: none;
+  border-radius: 0 var(--border-radius) var(--border-radius) 0;
+  width: 100%;
+  outline: none;
+  padding-left: 7px;
 }
 ul {
-	padding: 0;
-	margin: 0;
-	text-align: left;
-	list-style: none;
-	max-height: 200px;
-	overflow-y: scroll;
-	position: absolute;
-	top: 33px;
-	left: -1px;
-	background-color: #fff;
-	border: 1px solid #ccc;
-	width: 390px;
+  padding: 0;
+  margin: 0;
+  text-align: left;
+  list-style: none;
+  max-height: 200px;
+  overflow-y: scroll;
+  position: absolute;
+  top: 33px;
+  left: -1px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  width: 390px;
 }
 .dropdown {
-	display: flex;
-	flex-direction: column;
-	align-content: center;
-	justify-content: center;
-	position: relative;
-	padding: 7px;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+  position: relative;
+  padding: 7px;
 }
 .dropdown.open {
-	background-color: #f3f3f3;
+  background-color: #f3f3f3;
 }
 .dropdown:hover {
-	background-color: #f3f3f3;
+  background-color: #f3f3f3;
 }
 .dropdown-arrow {
-	transform: scaleY(0.5);
-	display: inline-block;
-	color: #666;
+  transform: scaleY(0.5);
+  display: inline-block;
+  color: #666;
 }
 .dropdown-item {
-	cursor: pointer;
-	padding: 4px 15px;
+  cursor: pointer;
+  padding: 4px 15px;
 }
 .dropdown-item:hover {
-	background-color: #f3f3f3;
+  background-color: #f3f3f3;
 }
 .flag {
-	width: 25px;
-	margin-left: 5px;
-	margin-right: 8px;
-	height: 1em;
+  width: 25px;
+  margin-left: 5px;
+  margin-right: 8px;
+  height: 1em;
 }
 .dropdown-menu.show {
   max-height: 300px;
@@ -116,10 +122,10 @@ export default {
     value: {
       type: String,
     },
-	placeholder: {
-	  default: "Enter a phone number",
-	  type: String,
-	}
+    placeholder: {
+      default: "Enter a phone number",
+      type: String,
+    }
   },
   mounted() {
     getCountry().then((res) => {
@@ -137,7 +143,7 @@ export default {
       phone: '',
       allCountries,
       activeCountry: { iso2: '' },
-	  open: false,
+      open: false,
     };
   },
   computed: {
@@ -212,44 +218,43 @@ export default {
       // Emit the response, includes phone, validity and country
       this.$emit('onInput', this.response);
     },
-	toggleDropdown: function() {
-		this.open = !this.open;
-	},
-	clickedOutside: function() {
-		this.open = false;
-	},
+    toggleDropdown: function () {
+      this.open = !this.open;
+    },
+    clickedOutside: function () {
+      this.open = false;
+    },
   },
-  directives:{
-        // Click-outside from BosNaufal: https://github.com/BosNaufal/vue-click-outside
-        'click-outside':{
-            bind: function (el, binding, vNode) {
-                // Provided expression must evaluate to a function.
-                if (typeof binding.value !== 'function') {
-                    var compName = vNode.context.name;
-                    var warn = '[Vue-click-outside:] provided expression ' + binding.expression + ' is not a function, but has to be';
-                    if (compName) {
-                        warn += 'Found in component ' + compName;
-                    }
-                    console.warn(warn);
-                }
-                // Define Handler and cache it on the element
-                var bubble = binding.modifiers.bubble;
-                var handler = function(e) {
-                    if (bubble || (!el.contains(e.target) && el !== e.target)) {
-                        binding.value(e)
-                    }
-                };
-                el.__vueClickOutside__ = handler;
-                // add Event Listeners
-                document.addEventListener('click', handler)
-            },
-            unbind: function (el, binding) {
-                // Remove Event Listeners
-                document.removeEventListener('click', el.__vueClickOutside__);
-                el.__vueClickOutside__ = null
-            }
-	}
-	}
+  directives: {
+    // Click-outside from BosNaufal: https://github.com/BosNaufal/vue-click-outside
+    'click-outside': {
+      bind: function (el, binding, vNode) {
+        // Provided expression must evaluate to a function.
+        if (typeof binding.value !== 'function') {
+          var compName = vNode.context.name;
+          var warn = '[Vue-click-outside:] provided expression ' + binding.expression + ' is not a function, but has to be';
+          if (compName) {
+            warn += 'Found in component ' + compName;
+          }
+          console.warn(warn);
+        }
+        // Define Handler and cache it on the element
+        var bubble = binding.modifiers.bubble;
+        var handler = function (e) {
+          if (bubble || (!el.contains(e.target) && el !== e.target)) {
+            binding.value(e)
+          }
+        };
+        el.__vueClickOutside__ = handler;
+        // add Event Listeners
+        document.addEventListener('click', handler)
+      },
+      unbind: function (el, binding) {
+        // Remove Event Listeners
+        document.removeEventListener('click', el.__vueClickOutside__);
+        el.__vueClickOutside__ = null
+      }
+    }
+  }
 };
 </script>
-

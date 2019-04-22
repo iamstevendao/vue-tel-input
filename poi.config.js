bbadda59b1628145d17169f9352c358abcf4a6c1
Steven Dao <iamstevendao@gmail.com>
2019-04-22 22:35:49 +1000

    Build for commonjs instead of umd

---
+++ poi.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  entry: './src/vue-tel-input.vue',
+  filename: {
+    js: 'vue-tel-input.js',
+    css: 'vue-tel-input.css'
+  },
+  sourceMap: false,
+  html: false,
+  format: 'cjs'
+}

0b9472dcb5962fe7e52e718c39fb229682f11c96
Steven Dao <iamstevendao@gmail.com>
2019-06-16 23:43:42 +1000

    Add hot reload for dev and remove unused config files

--- poi.config.js
+++ poi.config.js
@@ -1,10 +1,24 @@
-module.exports = {
-  entry: './src/vue-tel-input.vue',
-  filename: {
-    js: 'vue-tel-input.js',
-    css: 'vue-tel-input.css'
-  },
-  sourceMap: false,
-  html: false,
-  format: 'cjs'
+const config = {};
+
+if (process.env.NODE_ENV === 'production') {
+  Object.assign(config, {
+    entry: './src/vue-tel-input.vue',
+    filename: {
+      js: 'vue-tel-input.js',
+      css: 'vue-tel-input.css'
+    },
+    sourceMap: false,
+    html: false,
+    format: 'cjs'
+  });
+} else {
+  Object.assign(config, {
+    entry: './demo/index.js',
+    sourceMap: false,
+    html: {
+      template: './demo/index.html',
+    },
+  });
 }
+
+module.exports = config;

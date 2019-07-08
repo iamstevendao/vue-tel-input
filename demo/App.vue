7002a6e77e9d3b2bd406e0f8b9dd10e0900b3185
Steven Dao <iamstevendao@gmail.com>
2019-07-08 23:26:19 +1000

    Add playground in the demo page

--- demo/App.vue
+++ demo/App.vue
@@ -1,26 +1,54 @@
 <template>
   <div id="app">
-    <div style="width: 500px; margin: 20px auto;">
-      <vue-tel-input
-        :preferred-countries="['us', 'gb', 'ua']"
-        :valid-characters-only="true"
-        @input="onInput"/>
+    <template v-if="!showPlayground">
+      <h2 style="color: #999">made with &#x2764; by some <a
+        style="color: inherit"
+        href="https://github.com/EducationLink/vue-tel-input/graphs/contributors">awesome humans</a>.</h2>
+      <div style="width: 500px; margin: 20px auto;">
+        <vue-tel-input
+          :preferred-countries="['us', 'gb', 'ua']"
+          :valid-characters-only="true"
+          @input="onInput"/>
+      </div>
+      <div
+        v-if="phone.number"
+        style="color: #e83e8c">
+        <span>
+          Number:
+          <strong>{{ phone.number }}</strong>,&nbsp;
+        </span>
+        <span>
+          Is valid:
+          <strong>{{ phone.isValid }}</strong>,&nbsp;
+        </span>
+        <span>
+          Country:
+          <strong>{{ phone.country }}</strong>
+        </span>
+      </div>
+    </template>
+    <!-- built files will be auto injected -->
+    <div style="text-align: center; margin-top: 20px;">
+      View
+      <span style="font-size: 15px;">
+        [<a href="https://github.com/EducationLink/vue-tel-input">github</a>,
+        <a href="https://github.com/EducationLink/vue-tel-input/releases">changelogs</a>,
+        <a href="https://www.npmjs.com/package/vue-tel-input">npm</a>,
+        <a
+          href=""
+          @click.prevent="showPlayground = !showPlayground">
+        {{ showPlayground ? 'homepage' : 'playground' }}</a>].
+      </span>
     </div>
     <div
-      v-if="phone.number"
-      style="color: #e83e8c">
-      <span>
-        Number:
-        <strong>{{ phone.number }}</strong>,&nbsp;
-      </span>
-      <span>
-        Is valid:
-        <strong>{{ phone.isValid }}</strong>,&nbsp;
-      </span>
-      <span>
-        Country:
-        <strong>{{ phone.country }}</strong>
-      </span>
+      v-if="showPlayground"
+      style="margin-top: 20px;">
+      <iframe
+        src="https://codesandbox.io/embed/0yyyk45q7w?autoresize=1&fontsize=14&hidenavigation=1&module=%2Fsrc%2FApp.vue"
+        title="Vue Template"
+        allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media"
+        style="width:100%; height:80vh; border:0; border-radius: 4px; overflow:hidden;"
+        sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"/>
     </div>
   </div>
 </template>
@@ -30,6 +58,7 @@ export default {
   name: 'App',
   data() {
     return {
+      showPlayground: false,
       phone: {
         number: '',
         isValid: false,

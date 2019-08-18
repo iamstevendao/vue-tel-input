cc56334667bda1d02306184293816aa431dbb187
Steven Dao <iamstevendao@gmail.com>
2019-08-18 14:49:43 +1000

    Move styles down bottom

--- src/components/vue-tel-input.vue
+++ src/components/vue-tel-input.vue
@@ -54,97 +54,6 @@
   </div>
 </template>
 
-<style src="../assets/sprite.css"></style>
-<style scoped>
-li.last-preferred {
-  border-bottom: 1px solid #cacaca;
-}
-.iti-flag {
-  margin-right: 5px;
-  margin-left: 5px;
-}
-.dropdown-item .iti-flag {
-  display: inline-block;
-  margin-right: 5px;
-}
-.selection {
-  font-size: 0.8em;
-  display: flex;
-  align-items: center;
-}
-.vue-tel-input {
-  border-radius: 3px;
-  display: flex;
-  border: 1px solid #bbb;
-  text-align: left;
-}
-.vue-tel-input:focus-within {
-  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(102, 175, 233, 0.6);
-  border-color: #66afe9;
-}
-input {
-  border: none;
-  border-radius: 0 2px 2px 0;
-  width: 100%;
-  outline: none;
-  padding-left: 7px;
-}
-ul {
-  z-index: 1;
-  padding: 0;
-  margin: 0;
-  text-align: left;
-  list-style: none;
-  max-height: 200px;
-  overflow-y: scroll;
-  position: absolute;
-  top: 33px;
-  left: -1px;
-  background-color: #fff;
-  border: 1px solid #ccc;
-  width: 390px;
-}
-.dropdown {
-  display: flex;
-  flex-direction: column;
-  align-content: center;
-  justify-content: center;
-  position: relative;
-  padding: 7px;
-  cursor: pointer;
-}
-.dropdown.open {
-  background-color: #f3f3f3;
-}
-.dropdown:hover {
-  background-color: #f3f3f3;
-}
-.country-code {
-  color: #666;
-}
-.dropdown-arrow {
-  transform: scaleY(0.5);
-  display: inline-block;
-  color: #666;
-}
-.dropdown-item {
-  cursor: pointer;
-  padding: 4px 15px;
-}
-.dropdown-item.highlighted {
-  background-color: #f3f3f3;
-}
-.dropdown-menu.show {
-  max-height: 300px;
-  overflow: scroll;
-}
-.vue-tel-input.disabled .selection,
-.vue-tel-input.disabled .dropdown,
-.vue-tel-input.disabled input {
-  cursor: no-drop;
-}
-</style>
-
 <script>
 import PhoneNumber from 'awesome-phonenumber';
 import utils, { getCountry } from '../utils';
@@ -624,3 +533,94 @@ export default {
   },
 };
 </script>
+
+<style src="../assets/sprite.css"></style>
+<style scoped>
+li.last-preferred {
+  border-bottom: 1px solid #cacaca;
+}
+.iti-flag {
+  margin-right: 5px;
+  margin-left: 5px;
+}
+.dropdown-item .iti-flag {
+  display: inline-block;
+  margin-right: 5px;
+}
+.selection {
+  font-size: 0.8em;
+  display: flex;
+  align-items: center;
+}
+.vue-tel-input {
+  border-radius: 3px;
+  display: flex;
+  border: 1px solid #bbb;
+  text-align: left;
+}
+.vue-tel-input:focus-within {
+  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(102, 175, 233, 0.6);
+  border-color: #66afe9;
+}
+input {
+  border: none;
+  border-radius: 0 2px 2px 0;
+  width: 100%;
+  outline: none;
+  padding-left: 7px;
+}
+ul {
+  z-index: 1;
+  padding: 0;
+  margin: 0;
+  text-align: left;
+  list-style: none;
+  max-height: 200px;
+  overflow-y: scroll;
+  position: absolute;
+  top: 33px;
+  left: -1px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  width: 390px;
+}
+.dropdown {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+  position: relative;
+  padding: 7px;
+  cursor: pointer;
+}
+.dropdown.open {
+  background-color: #f3f3f3;
+}
+.dropdown:hover {
+  background-color: #f3f3f3;
+}
+.country-code {
+  color: #666;
+}
+.dropdown-arrow {
+  transform: scaleY(0.5);
+  display: inline-block;
+  color: #666;
+}
+.dropdown-item {
+  cursor: pointer;
+  padding: 4px 15px;
+}
+.dropdown-item.highlighted {
+  background-color: #f3f3f3;
+}
+.dropdown-menu.show {
+  max-height: 300px;
+  overflow: scroll;
+}
+.vue-tel-input.disabled .selection,
+.vue-tel-input.disabled .dropdown,
+.vue-tel-input.disabled input {
+  cursor: no-drop;
+}
+</style>

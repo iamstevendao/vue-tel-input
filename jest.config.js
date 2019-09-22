a22e34ff53ae9c6169462a3819dc9e2eeea684f1
Steven Dao <iamstevendao@gmail.com>
2019-09-22 23:13:01 +1000

    Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -19,9 +19,9 @@ module.exports = {
   "snapshotSerializers": [
     "jest-serializer-vue"
   ],
-  "testMatch": [
-    "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
-  ],
+  // "testMatch": [
+  //   "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
+  // ],
   "testURL": "http://localhost/",
   "watchPlugins": [
     "jest-watch-typeahead/filename",

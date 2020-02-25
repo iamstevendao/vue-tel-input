8314afd7f887fb8c54f2949815261e92e7345fff
merceyz <merceyz@users.noreply.github.com>
2020-02-25 18:09:39 +0100

    build: don't bundle runtime helpers

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset',
-  ],
+    [
+      '@vue/cli-plugin-babel/preset',
+      {
+        absoluteRuntime: false,
+        version: require('@babel/runtime/package.json').version
+      }
+    ]
+  ]
 };

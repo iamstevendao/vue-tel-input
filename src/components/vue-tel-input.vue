23f12f6a662f1bda7e098718df48acb008bfcd7d
Steven Dao <iamstevendao@gmail.com>
2020-04-01 22:19:29 +1100

    Move click outside directive to other file

--- src/components/vue-tel-input.vue
+++ src/components/vue-tel-input.vue
@@ -60,6 +60,7 @@
 <script>
 import PhoneNumber from 'awesome-phonenumber';
 import utils, { getCountry, setCaretPosition } from '../utils';
+import clickOutside from '../directives/click-outside';
 
 function getDefault(key) {
   const value = utils.options[key];
@@ -69,54 +70,10 @@ function getDefault(key) {
   return value;
 }
 
-// Polyfill for Event.path in IE 11: https://stackoverflow.com/a/46093727
-function getParents(node, memo) {
-  const parsedMemo = memo || [];
-  const { parentNode } = node;
-
-  if (!parentNode) {
-    return parsedMemo;
-  }
-
-  return getParents(parentNode, parsedMemo.concat(parentNode));
-}
-
 export default {
   name: 'VueTelInput',
   directives: {
-    // Click-outside by BosNaufal: https://github.com/BosNaufal/vue-click-outside
-    'click-outside': {
-      bind(el, binding, vNode) {
-        // Provided expression must evaluate to a function.
-        if (typeof binding.value !== 'function') {
-          const compName = vNode.context.name;
-          let warn = `[Vue-click-outside:] provided expression ${binding.expression} is not a function, but has to be`;
-          if (compName) {
-            warn += `Found in component ${compName}`;
-          }
-          console.warn(warn);
-        }
-        // Define Handler and cache it on the element
-        const { bubble } = binding.modifiers;
-        const handler = (e) => {
-          // Fall back to composedPath if e.path is undefined
-          const path = e.path
-            || (e.composedPath ? e.composedPath() : false)
-            || getParents(e.target);
-          if (bubble || (path.length && !el.contains(path[0]) && el !== path[0])) {
-            binding.value(e);
-          }
-        };
-        el.__vueClickOutside__ = handler;
-        // add Event Listeners
-        document.addEventListener('click', handler);
-      },
-      unbind(el) {
-        // Remove Event Listeners
-        document.removeEventListener('click', el.__vueClickOutside__);
-        el.__vueClickOutside__ = null;
-      },
-    },
+    clickOutside,
   },
   props: {
     value: {

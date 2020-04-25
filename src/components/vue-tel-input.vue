d22edd3337266c0a81ca91e037e51a7747d86167
sharvilak <sharvilak@e9ine.com>
2020-04-26 01:25:24 +0530

    code splitting : fetch css file only if enabledFlags is set to true
    - If enabledFlags is explicitly set to false, don't fetch the sprite.css. This will reduce the bundle size straight 114KB down.
    - If set to true (or by default), css file will be bundled separately in imported dynamically.

--- src/components/vue-tel-input.vue
+++ src/components/vue-tel-input.vue
@@ -329,9 +329,15 @@ export default {
         this.finishMounted = true;
       });
   },
-  created() {
+  async created() {
     if (this.value) {
       this.phone = this.value.trim();
+    }
+    // import sprite.css only if enabledFlags prop is set to true.
+    // In almost all cases this prop won't be changed once component is created.
+    // Hence it is safe keeping it under created. This saves almost 114 KB of file.
+    if (this.enabledFlags) {
+      await import('../assets/sprite.css');
     }
   },
   methods: {
@@ -564,7 +570,6 @@ export default {
 };
 </script>
 
-<style src="../assets/sprite.css"></style>
 <style>
 .vue-tel-input {
   border-radius: 3px;

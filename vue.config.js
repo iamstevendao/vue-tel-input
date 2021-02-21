7975552568edfd61e48761df17244400c913d88c
Steven Dao <iamstevendao@gmail.com>
2021-02-22 01:07:53 +1100

    Add component css and sprite css to build folder

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
 module.exports = {
   lintOnSave: true,
+  configureWebpack: {
+    plugins: [
+      new CopyPlugin([
+        { from: path.resolve(__dirname, 'src/assets/component.css'), to: 'css/' },
+        { from: path.resolve(__dirname, 'src/assets/sprite.css'), to: 'css/' },
+      ])
+    ]
+  },
 }

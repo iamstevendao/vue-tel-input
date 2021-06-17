bb481ba6801a2dc0c6b91e23706b9ef4988015e0
Adrien Foulon <adrien.foulon@tukan.hu>
2021-06-17 17:46:24 +0200

    Es7

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset',
+    [
+      '@vue/cli-plugin-babel/preset',
+      {
+        modules: false,
+        forceAllTransforms: true,
+        useBuiltIns: 'usage',
+        corejs: { version: 3, proposals: true },
+        exclude: ['transform-async-to-generator', 'transform-regenerator'],
+      },
+    ],
   ],
 };

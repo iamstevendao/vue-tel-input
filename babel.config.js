1bcf7e655628017e1a57a5e47597ae16d230178c
Adrien Foulon <adrien.foulon@tukan.hu>
2021-06-18 10:52:21 +0200

    Update deps and add second build mode

--- babel.config.js
+++ babel.config.js
@@ -2,12 +2,19 @@ module.exports = {
   presets: [
     [
       '@vue/cli-plugin-babel/preset',
-      {
-        modules: false,
-        forceAllTransforms: true,
+      process.env.MODERN ? {
+        targets: {
+          browsers: "last 2 chrome version, last 2 firefox version, last 2 edge version, last 1 safari version",
+        },
         useBuiltIns: 'usage',
         corejs: { version: 3, proposals: true },
         exclude: ['transform-async-to-generator', 'transform-regenerator'],
+      } : {
+        targets: {
+          browsers: "> .5%, last 2 versions, not dead",
+        },
+        useBuiltIns: 'usage',
+        corejs: { version: 3, proposals: true },
       },
     ],
   ],

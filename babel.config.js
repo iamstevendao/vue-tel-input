fa5b3cab98b6451fa26336bf646fbe8960f0b1d9
Martin Desrumaux <9059840+gnuletik@users.noreply.github.com>
2022-03-04 17:10:18 +0100

    fix(build): use browserslist config on all chunks (#318)
    The previous behavior that set browserslit config from babel.config.js
    did not work on files not provided in repo (e.g. webpack bookstrap).
    
    The new behavior copy the appropriate .browserslistrc file to the repo
    before build.
    
    Fixes IE 11 compatibility.

--- babel.config.js
+++ babel.config.js
@@ -3,16 +3,10 @@ module.exports = {
     [
       '@vue/cli-plugin-babel/preset',
       process.env.MODERN ? {
-        targets: {
-          browsers: "last 2 chrome version, last 2 firefox version, last 2 edge version, last 1 safari version",
-        },
         useBuiltIns: 'usage',
         corejs: { version: 3, proposals: true },
         exclude: ['transform-async-to-generator', 'transform-regenerator'],
       } : {
-        targets: {
-          browsers: "> .5%, last 2 versions, not dead",
-        },
         useBuiltIns: 'usage',
         corejs: { version: 3, proposals: true },
       },

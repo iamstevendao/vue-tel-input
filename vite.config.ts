2005a252421c1b4777bd36ec744e6d58ba91528b
Steven Dao <iamstevendao@gmail.com>
2023-04-11 23:18:21 +1000

    feat: Add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/vue-tel-input/',
   build: {
     cssCodeSplit: true,
     lib: {

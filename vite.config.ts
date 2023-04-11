fd11ab97c86c2e1b5bf84b1b52c9e5adbeb2ecbf
Steven Dao <iamstevendao@gmail.com>
2023-04-11 23:00:10 +1000

    feat: Improve css output file name

--- vite.config.ts
+++ vite.config.ts
@@ -6,22 +6,23 @@ import { resolve } from 'path'
 export default defineConfig({
   plugins: [vue()],
   build: {
+    cssCodeSplit: true,
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.js'),
       name: 'VueTelInput',
-      // the proper extensions will be added
       fileName: 'vue-tel-input',
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['vue'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           vue: 'Vue',
+        },
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.css')) {
+            return 'vue-tel-input.css';
+          }
+          return assetInfo.name;
         },
       },
     },

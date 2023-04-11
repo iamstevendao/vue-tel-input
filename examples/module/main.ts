aa68eaf9e0521b3b12b573770210acb50cd8c551
Steven Dao <iamstevendao@gmail.com>
2023-04-11 23:33:25 +1000

    feat: Remove examples folder

--- examples/module/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue';
-import { VueTelInput } from '../../dist/vue-tel-input.es.js';
-import '../../dist/vue-tel-input.css';
-
-const app = createApp(App);
-app.component('VueTelInput', VueTelInput);
-app.mount('#app');

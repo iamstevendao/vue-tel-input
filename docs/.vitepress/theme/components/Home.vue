32832e1d9a4bb3be37ea120ae9682b8a3ea7e848
reslear <reslear@gmail.com>
2025-01-06 19:58:06 +0100

    refactor(docs): components to vue setup ts

--- docs/.vitepress/theme/components/Home.vue
+++ docs/.vitepress/theme/components/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <main class="home">
     <header class="hero vp-doc">
-      <img :src="withBase('hero.png')"
-           alt="vue-tel-input" />
+      <img :src="withBase('hero.png')" alt="vue-tel-input" />
       <h1>International Telephone Input with Vue</h1>
     </header>
 
     <section class="description vp-doc">
       <div>
-        <a href="https://www.npmjs.com/package/vue-tel-input"
-              ><img src="https://img.shields.io/npm/dt/vue-tel-input.svg" /></a>&nbsp;
-        <a href="https://github.com/iamstevendao/vue-tel-input"
-              ><img src="https://img.shields.io/github/stars/iamstevendao/vue-tel-input.svg"
-            /></a>
+        <a href="https://www.npmjs.com/package/vue-tel-input">
+          <img src="https://img.shields.io/npm/dt/vue-tel-input.svg" />
+        </a>&nbsp;
+        <a href="https://github.com/iamstevendao/vue-tel-input">
+          <img src="https://img.shields.io/github/stars/iamstevendao/vue-tel-input.svg" />
+        </a>
       </div>
       <h3>
         made with &#x2764; by some
@@ -22,14 +22,12 @@
 
     <section class="content">
       <div style="width: 500px; margin: 0 auto;">
-        <vue-tel-input v-model="phoneModel"
-                       v-bind="options"
-                       @onInput="onInput" />
+        <VueTelInput v-model="phoneModel" v-bind="options" @onInput="onInput" />
       </div>
       <code v-if="phoneObject.formatted">
         <span>Formatted: <b>{{ phoneObject.formatted }}</b>,&nbsp;</span>
         <span>Is valid: <b>{{ phoneObject.valid || 'false' }}</b>,&nbsp;</span>
-        <span>Country: <b>{{ phoneObject.country?.name }}</b></span>
+        <span>Country: <b>{{ phoneObject.country }}</b></span>
       </code>
     </section>
 
@@ -40,62 +38,147 @@
       >
         <span>{{ showOptions ? "Hide" : "Show" }} options</span>
       </a>
-      <div v-if="showOptions"
-           class="formResults">
+      <div v-if="showOptions" class="formResults">
         <div class="form">
-          <div v-for="field in otherFields"
-               :key="field.model"
-               :class="field.containerClasses">
-            <form-input v-bind="field.bind"
-                        :model="options"
-                        :modelName="field.model"
-                        :description="field.description"
-                        :label="field.label"
-                        :type="field.type" />
+          <div
+            v-for="field in otherFields"
+            :key="field.model"
+          >
+            <form-input
+              v-bind="field.bind"
+              :model="options"
+              :modelName="field.model"
+              :description="field.description"
+              :label="field.label"
+              :type="field.type"
+            />
           </div>
-          <div v-for="field in inputFields"
-               :key="field.model"
-               :class="field.containerClasses">
-            <form-input v-bind="field.bind"
-                        :model="options.inputOptions"
-                        :description="field.description"
-                        :modelName="field.model"
-                        :label="field.label"
-                        :type="field.type" />
+          <div
+            v-for="field in inputFields"
+            :key="field.model"
+          >
+            <form-input
+              v-bind="field.bind"
+              :model="options.inputOptions"
+              :description="field.description"
+              :modelName="field.model"
+              :label="field.label"
+              :type="field.type"
+            />
           </div>
-          <div v-for="field in dropdownFields"
-               :key="field.model"
-               :class="field.containerClasses">
-            <form-input v-bind="field.bind"
-                        :model="options.dropdownOptions"
-                        :description="field.description"
-                        :modelName="field.model"
-                        :label="field.label"
-                        :type="field.type" />
+          <div
+            v-for="field in dropdownFields"
+            :key="field.model"
+          >
+            <form-input
+              v-bind="field.bind"
+              :model="options.dropdownOptions"
+              :description="field.description"
+              :modelName="field.model"
+              :label="field.label"
+              :type="field.type"
+            />
           </div>
           <div style="margin-top: 15px">
             <a
-                  style="color: #999999"
-                  href="https://iamstevendao.github.io/vue-tel-input/usage/props.html"
-                >
-                  <span>...more</span>
-                </a>
+              style="color: #999999"
+              href="https://iamstevendao.github.io/vue-tel-input/usage/props.html"
+            >
+              <span>...more</span>
+            </a>
           </div>
         </div>
         <div class="results">
-          <pre>{{ JSON.stringify(this.phoneObject, null, 2) }}</pre>
+          <pre>{{ JSON.stringify(phoneObject, null, 2) }}</pre>
         </div>
       </div>
     </section>
   </main>
 </template>
 
-<script>
+<script lang="ts" setup>
 import { withBase } from 'vitepress';
 import FormInput from './FormInput.vue';
 import { allProps, defaultOptions } from '../../../../src/utils';
+import { reactive, computed, ref } from 'vue';
+import type { PhoneMeta, } from 'vue-tel-input';
+import { VueTelInput } from 'vue-tel-input';
 
-function getFormConfig(field) {
+const phoneModel = ref('');
+const showOptions = ref(false);
+
+const phoneObject = reactive<Pick<PhoneMeta, 'formatted' | 'valid' | 'country'>>({
+  formatted: '',
+  valid: false,
+  country: undefined
+});
+
+const options = reactive({
+  ...defaultOptions,
+  inputOptions: {
+    ...defaultOptions.inputOptions,
+    showDialCode: true,
+  },
+  mode: 'international',
+  validCharactersOnly: true,
+  dropdownOptions: {
+    ...defaultOptions.dropdownOptions,
+  },
+});
+
+const fields = computed<{
+  model: string;
+  label: string;
+  description: string;
+  type?: 'input' | 'checkbox' | 'radio';
+  bind?: Record<string, any>;
+}[]>(() =>
+  allProps
+    .filter(({ inDemo }) => inDemo)
+    .map((prop) => ({
+      model: prop.name,
+      label: prop.name,
+      description: prop.description,
+      ...getFormConfig(prop),
+    }))
+);
+
+const dropdownFields = computed(() =>
+  fields.value
+    .filter(({ model }) => model.includes('dropdownOptions'))
+    .map((field) => ({
+      ...field,
+      model: field.model.split('.')[1],
+    }))
+);
+
+const inputFields = computed(() =>
+  fields.value
+    .filter(({ model }) => model.includes('inputOptions'))
+    .map((field) => ({
+      ...field,
+      model: field.model.split('.')[1],
+    }))
+);
+
+const otherFields = computed(() =>
+  fields.value.filter(
+    ({ model }) =>
+      !model.includes('dropdownOptions') && !model.includes('inputOptions')
+  )
+);
+
+function onInput(formattedNumber: string, phoneObj: any) {
+  console.log('onInput', formattedNumber, phoneObj);
+  phoneObject.formatted = formattedNumber;
+  phoneObject.valid = phoneObj.valid;
+  phoneObject.country = phoneObj.country;
+}
+
+function getFormConfig(field: any): {
+  type: 'input' | 'checkbox' | 'radio';
+  bind?: Record<string, any>;
+} | undefined {
   if (field.type.name === 'Boolean') {
     return { type: 'checkbox' };
   }
@@ -106,71 +189,9 @@ function getFormConfig(field) {
     return { type: 'input' };
   }
 }
-
-export default {
-  name: 'Home',
-  components: { FormInput },
-  data() {
-    return {
-      phoneModel: '',
-      showOptions: false,
-      phoneObject: {
-        formatted: '',
-        valid: false,
-        country: undefined,
-      },
-      options: {
-        ...defaultOptions,
-        inputOptions: {
-          ...defaultOptions.inputOptions,
-          showDialCode: true,
-        },
-        mode: 'international',
-        validCharactersOnly: true,
-      },
-      fields: allProps
-        .filter(({ inDemo }) => inDemo)
-        .map((prop) => ({
-          model: prop.name,
-          label: prop.name,
-          description: prop.description,
-          ...getFormConfig(prop),
-        })),
-    };
-  },
-  computed: {
-    dropdownFields() {
-      return this.fields
-        .filter(({ model }) => model.includes('dropdownOptions'))
-        .map((field) => ({
-          ...field,
-          model: field.model.split('.')[1],
-        }));
-    },
-    inputFields() {
-      return this.fields
-        .filter(({ model }) => model.includes('inputOptions'))
-        .map((field) => ({
-          ...field,
-          model: field.model.split('.')[1],
-        }));
-    },
-    otherFields() {
-      return this.fields
-        .filter(({ model }) => !model.includes('dropdownOptions') && !model.includes('inputOptions'));
-    },
-  },
-  methods: {
-    withBase,
-    onInput(formattedNumber, phoneObject) {
-      console.log('onInput', formattedNumber, phoneObject);
-      this.phoneObject = phoneObject;
-    },
-  },
-};
 </script>
 
-<style>
+<style scoped>
 @import url('https://fonts.googleapis.com/css?family=Nunito+Sans&display=swap');
 
 .home {
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,13 +1,13 @@
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue';
-import VueTelInput from 'vue-tel-input';
+//import VueTelInput from 'vue-tel-input';
 import 'vue-tel-input/vue-tel-input.css';
 
 export default {
   extends: DefaultTheme,
   Layout,
   enhanceApp({ app }) {
-    app.use(VueTelInput);
+    //app.use(VueTelInput);
   },
 } satisfies Theme
